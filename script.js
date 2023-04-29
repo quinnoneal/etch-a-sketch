746eefca2a2fb60ddd7be0236f2917f9ebfb4e26
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-28 19:25:25 -1000

    update button styling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
 function createGrid(size) {
     const gridContainer = document.querySelector('.grid-container');
+    const squareSize = getSquareSize(size);
 
     for(let i = 0; i < size; i++) {
         const colDiv = document.createElement('div');
@@ -7,26 +9,17 @@ function createGrid(size) {
         for(let j = 0; j < size; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            // name each square accordingly
             square.classList.add(`col-${i}`);
             square.classList.add(`row-${j}`);
+            // size each square accordingly
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
             colDiv.appendChild(square);
         }
         gridContainer.appendChild(colDiv);
     }
 }
-
-createGrid(16);
-initializeDrawing();
-
-// setup custom grid size button
-const changeGridSizeBtn = document.querySelector('.grid-size-button');
-changeGridSizeBtn.addEventListener('click', () => {
-    const size = Number(prompt("Enter Grid Size"));
-    // reset current grid
-    removeCurrentGrid();
-    createGrid(size);
-    initializeDrawing();
-})
 
 // drawing funcionality
 function initializeDrawing() {
@@ -39,9 +32,30 @@ function initializeDrawing() {
     })    
 }
 
+// removes old grid upon new sized grid
 function removeCurrentGrid() {
     const gridContainer = document.querySelector('.grid-container');
     while(gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
 }
+
+function getSquareSize(size) {
+    const MAX_GRID_SIZE = 500;
+    const pixelsPerSquare = MAX_GRID_SIZE / size;
+    return pixelsPerSquare;
+}
+
+// init grid
+createGrid(16);
+initializeDrawing();
+
+// setup grid size button
+const changeGridSizeBtn = document.querySelector('.grid-size-button');
+changeGridSizeBtn.addEventListener('click', () => {
+    const size = Number(prompt("Enter Grid Size"));
+    // reset current grid
+    removeCurrentGrid();
+    createGrid(size);
+    initializeDrawing();
+})
