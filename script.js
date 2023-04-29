6b0639b64516d4c3bffff11bb417c7418bd31049
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-28 19:11:30 -1000

    turn existing code into functions so when user changes grid size, functionality will still be there

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-function createGrid() {
+function createGrid(size) {
     const gridContainer = document.querySelector('.grid-container');
 
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < size; i++) {
         const colDiv = document.createElement('div');
         colDiv.classList.add(`col-${i}`);
-        for(let j = 0; j < 16; j++) {
+        for(let j = 0; j < size; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
-            square.classList.add(`row-${i}`);
+            square.classList.add(`col-${i}`);
+            square.classList.add(`row-${j}`);
             colDiv.appendChild(square);
         }
         gridContainer.appendChild(colDiv);
     }
 }
 
-createGrid();
+createGrid(16);
+initializeDrawing();
+
+// setup custom grid size button
+const changeGridSizeBtn = document.querySelector('.grid-size-button');
+changeGridSizeBtn.addEventListener('click', () => {
+    const size = Number(prompt("Enter Grid Size"));
+    // reset current grid
+    removeCurrentGrid();
+    createGrid(size);
+    initializeDrawing();
+})
+
+// drawing funcionality
+function initializeDrawing() {
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'black';
+        })
+    })    
+}
+
+function removeCurrentGrid() {
+    const gridContainer = document.querySelector('.grid-container');
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
